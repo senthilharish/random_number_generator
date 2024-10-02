6773f90c56b183c800bec161ca88628fc9a932e4
senthilharish <sindhuharish2802@gmail.com>
2024-10-02 09:30:39 +0530

    adding text input

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-
 
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+
+int a = 0;
+int b = 0;
+
+final _textcontroller1 = TextEditingController();
+final _textcontroller2 = TextEditingController();
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(title:const Text("random number"),),
-        body: Column(
-          children: [
-            
-          ],
-        ),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Random Number"),
+      ),
+      body: Column(
+        children: [
+          input(_textcontroller1),
+          button(a,_textcontroller1),
+          input(_textcontroller2),
+          button(b,_textcontroller2),
+          SizedBox(
+            height: 20,
+          ),
+          
+        ],
+      ),
+    ));
   }
+
+
+  TextField input(control) {
+    return TextField(
+          controller: control,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              hintText: "Enter the starting number:"),
+          keyboardType: TextInputType.number, // Ensure only numbers are entered
+        );
+  }
+    MaterialButton button(value,control1) {
+    return MaterialButton(
+          onPressed: () {
+            setState(() {
+              // Convert the entered text to an integer
+              value = int.tryParse(control1.text) ?? 0;
+              print(value);
+            });
+          },
+          color: Colors.blueGrey,
+          child: const Text("click"),
+        );
+  }
+
 }
